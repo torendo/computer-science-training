9e300df0fbc806e942018c147abd1c9a0b39ec72
nekro20 <toren4eg@gmail.com>
2020-02-19 00:32:20 +0300

    Graph page in progress

--- client/components/itemsGraph.js
+++ client/components/itemsGraph.js
@@ -13,6 +13,7 @@ export class XItemsGraph extends LitElement {
   static get properties() {
     return {
       items: {type: Array},
+      connections: {type: Array},
       marker: {type: Object},
       clickFn: {type: Function}
     };
@@ -21,14 +22,15 @@ export class XItemsGraph extends LitElement {
   constructor() {
     super();
     this.items = [];
-    this.dragPositions = null;
+    this.connections = new Map();
+    this.dragOpts = null;
   }
 
   render() {
     const items = this.items.map(item => svg`
       <g fill="${item.color}">
-        ${this.drawConnectionLines(item)}
         <g
+          class="clickable"
           @click=${this.clickHandler.bind(this, item)}"
           @mousedown="${(e) => this.dragstartHandler(e, item)}"
           @mouseup="${(e) => this.dragendHandler(e, item)}"
@@ -37,15 +39,17 @@ export class XItemsGraph extends LitElement {
           <text class="value" x="${item.x}" y="${item.y + 2}" text-anchor="middle" alignment-baseline="middle">${item.value}</text>
         </g>
       </g>
+      ${this.drawConnections(item)}
     `);
     return svg`
       <svg viewBox="0 0 600 400" xmlns="http://www.w3.org/2000/svg"
         @dblclick=${this.dblclickHandler}
         @mousedown="${(e) => e.preventDefault()}"
+        @mouseup="${this.dragendHandler}"
         @mousemove="${this.dragHandler}"
       >
-        ${this.dragPositions != null ? svg`
-          <line class="line" x1="${this.dragPositions.dragItem.x}" y1="${this.dragPositions.dragItem.y}" x2="${this.dragPositions.x}" y2="${this.dragPositions.y}">
+        ${this.dragOpts != null ? svg`
+          <line class="line" x1="${this.dragOpts.dragItem.x}" y1="${this.dragOpts.dragItem.y}" x2="${this.dragOpts.x}" y2="${this.dragOpts.y}">
         ` : ''}
         ${items}
       </svg>
@@ -53,14 +57,22 @@ export class XItemsGraph extends LitElement {
   }
 
 
-  drawConnectionLines(item) {
-    return svg`
-    
-    `;
+  drawConnections(item) {
+    //TODO: draw connections below all items, so rework this
+    const connections = this.connections.get(item);
+    const lines = [];
+    if (connections != null) {
+      for (let connection of connections) {
+        lines.push(svg`
+          <line class="line" x1="${item.x}" y1="${item.y}" x2="${connection.x}" y2="${connection.y}">
+        `);
+      }
+    }
+    return lines;
   }
 
   dblclickHandler(e) {
-    if (this.dragPositions != null) return;
+    if (this.dragOpts != null) return;
     const index = this.items.length;
     const item = new PlacedItem({
       index,
@@ -72,7 +84,7 @@ export class XItemsGraph extends LitElement {
   }
 
   dragstartHandler(e, item) {
-    this.dragPositions = {
+    this.dragOpts = {
       initialX: e.clientX,
       initialY: e.clientY,
       dragItem: item
@@ -80,21 +92,31 @@ export class XItemsGraph extends LitElement {
   }
 
   dragHandler(e) {
-    if (this.dragPositions == null) return;
-    this.dragPositions.x = e.offsetX;
-    this.dragPositions.y = e.offsetY;
+    if (this.dragOpts == null) return;
+    this.dragOpts.x = e.offsetX;
+    this.dragOpts.y = e.offsetY;
     this.requestUpdate();
   }
 
   dragendHandler(e, item) {
-    this.dragPositions = null;
+    if (this.dragOpts && item && item !== this.dragOpts.dragItem) {
+      const dragItem = this.dragOpts.dragItem;
+      if (this.connections.has(dragItem)) {
+        this.connections.get(dragItem).add(item);
+      } else {
+        const value = new Set();
+        value.add(item);
+        this.connections.set(dragItem, value);
+      }
+    }
+    this.dragOpts = null;
     this.requestUpdate();
   }
 
   clickHandler(item) {
     if (this.clickFn != null) {
 
-      //l
+      //TODO: marker change position
 
       return this.clickFn(item);
     }
@@ -116,6 +138,9 @@ XItemsGraph.styles = css`
   }
   .item.marked {
     stroke: red;
+  }
+  .clickable {
+    cursor: pointer;
   }
   .value {
     font: normal 13px sans-serif;
